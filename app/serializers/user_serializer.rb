926630baabfea3d2b150eed74d42559d9cc808d0
dowpm <mcdowpm@gmail.com>
2019-04-30 22:18:30 -0400

    fixe bug remove image key

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :full_name, :seller, :image
+  attributes :id, :email, :full_name, :seller
 
   has_one :cart, dependent: :destroy
   has_many :orders
