d834b524c008f88720ae8408931862309b5b363a
dowpm <mcdowpm@gmail.com>
2019-04-30 21:24:12 -0400

    edit class to className

--- client/src/components/UserLeftMenu.js
+++ client/src/components/UserLeftMenu.js
@@ -4,16 +4,16 @@ export default class UserLeftMenu extends Component{
 
     render(){
         return(
-            <div class="col-lg-3">
-                <div class="card sidebar-menu">
-                    <div class="card-header">
-                    <h3 class="h4 card-title">Customer section</h3>
+            <div className="col-lg-3">
+                <div className="card sidebar-menu">
+                    <div className="card-header">
+                    <h3 className="h4 card-title">Customer section</h3>
                     </div>
-                    <div class="card-body">
-                    <ul class="nav nav-pills flex-column">
-                        <a href="customer-orders.html" class="nav-link active"><i class="fa fa-list"></i> My orders</a>
-                        <a href="customer-account.html" class="nav-link"><i class="fa fa-user"></i> My account</a>
-                        <a href="index.html" class="nav-link"><i class="fa fa-sign-out"></i> Logout</a>
+                    <div className="card-body">
+                    <ul className="nav nav-pills flex-column">
+                        <a href="customer-orders.html" className="nav-link active"><i className="fa fa-list"></i> My orders</a>
+                        <a href="customer-account.html" className="nav-link"><i className="fa fa-user"></i> My account</a>
+                        <a href="index.html" className="nav-link"><i className="fa fa-sign-out"></i> Logout</a>
                     </ul>
                     </div>
                 </div>
