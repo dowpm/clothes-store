28a70e09292af5f1a7700d4c4cd2b73844fcb8ad
dowpm <mcdowpm@gmail.com>
2019-05-01 02:42:20 -0400

    create add_product case reducer

--- client/src/redux/reducers/productsReducer.js
+++ client/src/redux/reducers/productsReducer.js
@@ -8,8 +8,9 @@ export default (state = initialState, action) => {
             console.log(action)
             return {...state, products: action.payload}
 
-        case 'ADD_PRODUCTS':
-            return {...state, products: state.products.append(action.payload)}
+        case 'ADD_PRODUCT':
+        // console.log(action.payload)
+            return {...state, products: [...state.products,action.payload]}
 
         default:
             return state
