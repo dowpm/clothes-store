9fd0a930fb92fc63c6a076f558cf0e6f7230c9af
dowpm <mcdowpm@gmail.com>
2019-04-30 22:17:34 -0400

    permit params key and use set_product and show action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+    before_action :set_product, only: [:show]
     def index
         @products = Product.all
         render json: @products.map {|product|
@@ -11,6 +12,32 @@ class Api::V1::ProductsController < ApplicationController
             end
         }
     end
+    
+    def show
+        if @product.images.attached?
+            render json: @product.as_json.merge({
+                images: @product.images.map {|img| url_for(img)}
+            })
+        else
+            render json: @product
+        end
+    end
+
+    def create
+        product = Product.new(product_params)
+        binding.pry
+        if product.save
+            if product.images.attached?
+                render json: product.as_json.merge({
+                    images: product.images.map {|img| url_for(img)}
+                }), status: 201
+            else
+                render json: product, status: 201
+            end
+        else
+            render json: product.errors, status: 500
+        end
+    end
 
     private
 
@@ -19,6 +46,10 @@ class Api::V1::ProductsController < ApplicationController
     end
 
     def product_params
-        params.permit(:full_name, :email, :password, :seller, images: [])
+        params.permit(
+            :name, :description, :quantity,
+            :section,:category_id,
+            :user_id, :price, images: []
+        )
     end
 end
