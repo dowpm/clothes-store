5b2073dc6b6c54a653cf70e7ed6ac91a1e02931d
dowpm <mcdowpm@gmail.com>
2019-05-03 15:43:40 -0400

    delete useless things

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import logo from './logo.svg';
+
 import './vendor/bootstrap/css/bootstrap.min.css';
 import './vendor/font-awesome/css/font-awesome.min.css'
 import './css/style.default.css'
-// import './vendor/owl.carousel/assets/owl.carousel.css'
-// import './vendor/owl.carousel/assets/owl.theme.default.css'
 import Header from './components/Header'
-// import All from './components/All'
 import Footer from './components/Footer'
 import NotFound from './components/NotFound'
-// import Products from './containers/Products'
 import Admin from './containers/Admin'
-// import Orders from './components/Orders'
-// import Order from './components/Order'
+
 import AllProducts from './containers/AllProducts'
 import Home from './components/Home'
 import { Alert } from './helpers/notifications';
 
-// import './App.css';
-
 class App extends Component {
   render() {
-    // const path = window.location.pathname
-    // debugger
+
     return (
       <Router>
         <Header item={'path'}/>
@@ -38,14 +30,6 @@ class App extends Component {
           <Route  path="/admin/products/:id?" component={Admin} />
           <Route component={NotFound} />
 
-          {/* <Route exact path="/register" component={All} />
-          <Route exact path="/products/men" component={Products} />
-          <Route exact path="/products/women" component={Products} />
-          <Route exact path="/products/kids" component={Products} />
-          <Route exact path="/orders" component={Orders} />
-          <Route exact path="/order" component={Order} />
-          <Route exact path="/basket" component={Cart} />
-          <Route exact path="/404" component={NotFound} /> */}
         </Switch>
 
                 </div>
