209791991b57781216e1b33e98b6e687d8ec74b2
dowpm <mcdowpm@gmail.com>
2019-05-02 21:43:58 -0400

    comment basket icon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -56,7 +56,7 @@ const NavBar = ({item}) => {
                         <button type="button" data-toggle="collapse" data-target="#navigation" className="btn btn-outline-secondary navbar-toggler">
                         <span className="sr-only">Toggle navigation</span><i className="fa fa-align-justify"></i>
                         </button>
-                        <a href="basket" className="btn btn-outline-secondary navbar-toggler"><i className="fa fa-shopping-cart"></i></a>
+                        {/* <a href="basket" className="btn btn-outline-secondary navbar-toggler"><i className="fa fa-shopping-cart"></i></a> */}
                     </div>
                     <div id="navigation" className="collapse navbar-collapse">
                         <ul className="navbar-nav mr-auto">
