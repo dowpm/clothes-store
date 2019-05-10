c276f75b440b6b9d699244198bbb6c95b033f3d3
dowpm <mcdowpm@gmail.com>
2019-05-10 07:22:50 -0400

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :categories
       resources :products
-      resources :carts
-      resources :users, only: [:index, :create]
-      resources :orders
+      post '/login',to: 'session#login'
     end
   end
 end
