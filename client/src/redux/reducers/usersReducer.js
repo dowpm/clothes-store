b7388449d332617e0c66d4ff5c89f1fb91984f84
dowpm <mcdowpm@gmail.com>
2019-05-10 07:36:55 -0400

    edit user reducer

--- client/src/redux/reducers/usersReducer.js
+++ client/src/redux/reducers/usersReducer.js
@@ -6,13 +6,20 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case 'SET_STORAGE':
+        return {...state, currentUser: JSON.parse(localStorage.getItem('current_user'))}
+        
         case 'LOG_IN':
-        console.log('inside sigup reducer')
-        console.log(action)
+        localStorage.setItem('current_user', JSON.stringify(action.payload));
         return {...state, currentUser: action.payload}
 
         case 'LOG_OUT':
+        localStorage.removeItem('current_user');
         return {...state, currentUser: {}}
+
+        case 'SIGNUP':
+        localStorage.setItem('current_user', JSON.stringify(action.payload));
+        return {...state, currentUser: action.payload}
 
         default:
             return state
