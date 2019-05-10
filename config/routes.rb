fafaa7197ebadfa53e53da98cf8699ef14fcc40c
dowpm <mcdowpm@gmail.com>
2019-05-10 11:46:17 -0400

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products
+      resources :users
       post '/login',to: 'session#login'
     end
   end
